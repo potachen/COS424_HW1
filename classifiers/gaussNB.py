0591d66c9746df630a647542dc49c98e6e8e37ca
Hugh Wilson <hughwilson1@hotmail.co.uk>
2016-02-15 22:32:01 -0500

    Added file for the naive bayesian gaussian classifier

---
+++ classifiers/gaussNB.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python
+
+"""
+Use a Gaussian Naive Bayesian classifier to Classify a feature space
+into classes using a set of vectors and lables
+"""
+
+import numpy as np
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # some test things
+    pass
