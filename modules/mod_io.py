47840612565ed8685204f963a105d24dfb623852
Hugh Wilson <hughwilson1@hotmail.co.uk>
2016-02-15 22:01:53 -0500

    cosmetic compatibility change

--- modules/mod_io.py
+++ modules/mod_io.py
@@ -30,10 +30,10 @@ if __name__ == '__main__':
     ### --- Paths for Different Systems --- ###
     ptc_mbp = '/Users/po-tachen/Desktop/COS424/HW1/voxResources-master/data'
     ptc_ubu = '/home/potachen/Desktop/COS424/HW1/voxResources-master/data'
-    hmbp = '/Users/hugh/Google Drive/Hugh/PhD/princeton/COS424/assignments/1/data/voxResources/data'
+    hsw_mbp = '/Users/hugh/Google Drive/Hugh/PhD/princeton/COS424/assignments/1/data/voxResources/data'
 
     if platform.node() == 'nat-oitwireless-inside-vapornet100-d-1862.Princeton.EDU':
-        data_path = hmbp
+        data_path = hsw_mbp
     else:
         data_path = ptc_mbp if platform.system() == 'Darwin' else ptc_ubu
 
