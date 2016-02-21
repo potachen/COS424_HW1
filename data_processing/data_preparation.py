f84b38476a2adf9fe0479b4006dd1fe8f39c8a02
Po-Ta Chen <ptc.phys.cs@gmail.com>
2016-02-21 18:12:14 -0500

    mbp-2016-02-21: first version which could generate dataset for training, validating, and testing.

--- data_processing/data_preparation.py
+++ data_processing/data_preparation.py
@@ -8,50 +8,64 @@ import math
 import data_processing.io_interface as dp_io
 
 
-def get_data():
-
-    file_list = dp_io.get_file_paths(data_path)
-    data_training_matrix = np.zeros(shape=(file_list.__len__() * 80, 1198), dtype=np.float64)
-    class_training_vector = np.zeros(shape=(file_list.__len__() * 80, 1), dtype=np.float64)
-    training_feature_array = np.array([])
-    training_class_array = np.array([])
-
-    for i in range(file_list.__len__()):
-        path = file_list[i][0] + os.path.sep
-        file_names = file_list[i][1]
-        random.shuffle(file_names)
-        data_training_len = int(math.ceil(file_names.__len__() * 0.8))
-        for j in range(0, data_training_len):
-            features, classes = get_feature_class(dp_io.read_mat(path + file_names[j]), feature='mfc')
-
-            if features.shape[0] < 1198:
-                print path + file_names[j]
-                print features.shape
-                for d in range(1198 - features.shape[0]):
-                    features = np.concatenate((features, np.array([0])))
-
-            for k in range(1198):
-                data_training_matrix[j + i * data_training_len, k] = features[k]
-                class_training_vector[j + i * data_training_len] = classes
-
-            # data_training_matrix[j + i * data_training_len], class_training_vector[j + i * data_training_len] = \
-            #     get_feature_class(dp_io.read_mat(path + file_names[j]), feature='mfc')
-            # training_feature_array = np.concatenate((training_feature_array, features))
-            # training_class_array = np.concatenate((training_class_array, classes))
-
-    # print data_training_matrix
-    # print class_training_vector
-    # print training_feature_array
-    # print training_class_array
-
-    return data_training_matrix, class_training_vector
-
-
-def get_feature_class(mat_struct, feature='zerocross'):
+def get_feature_class_from_mat(mat_struct, feature='zerocross'):
     return mat_struct['DAT'][feature][0, 0][0], mat_struct['DAT']['class'][0, 0][0]
 
 
+def get_feature_class_matrix(data_range_list, path, file_name_list, feature_matrix, class_vector):
+
+    data_len = (data_range_list[1] - data_range_list[0])
+
+    for j in range(data_range_list[0], data_range_list[1]):
+            features, classes = get_feature_class_from_mat(dp_io.read_mat(path + file_name_list[j % data_len]),
+                                                           feature='zerocross')
+            if features.shape[0] < 1198:
+                # print path + file_name_list[j]
+                # print features.shape
+                for d in range(1198 - features.shape[0]):
+                    features = np.concatenate((features, np.array([0])))
+
+            # print j + class_ind * data_len
+            for k in range(1198):
+                feature_matrix[j, k] = features[k]
+                class_vector[j] = classes
+
+
+def get_data(ratio):
+
+    class_file_list = dp_io.get_file_paths(data_path)
+
+    feature_training_matrix = np.zeros(shape=(class_file_list.__len__() * 100 * ratio[0], 1198), dtype=np.float64)
+    class_training_vector = np.zeros(shape=(class_file_list.__len__() * 100 * ratio[0], 1), dtype=np.float64)
+    feature_validating_matrix = np.zeros(shape=(class_file_list.__len__() * 100 * ratio[1], 1198), dtype=np.float64)
+    class_validating_vector = np.zeros(shape=(class_file_list.__len__() * 100 * ratio[1], 1), dtype=np.float64)
+    feature_testing_matrix = np.zeros(shape=(class_file_list.__len__() * 100 * ratio[2], 1198), dtype=np.float64)
+    class_testing_vector = np.zeros(shape=(class_file_list.__len__() * 100 * ratio[2], 1), dtype=np.float64)
+
+    for class_ind in range(class_file_list.__len__()):
+
+        path = class_file_list[class_ind][0] + os.path.sep
+        file_name_list = class_file_list[class_ind][1]
+        random.shuffle(file_name_list)
+
+        print path
+
+        training_len = int(math.ceil(file_name_list.__len__() * ratio[0]))
+        validating_len = int(math.ceil(file_name_list.__len__() * ratio[1]))
+        testing_len = int(math.ceil(file_name_list.__len__() * ratio[2]))
+
+        get_feature_class_matrix([0 + class_ind * training_len, training_len * (1 + class_ind)],
+                                 path, file_name_list, feature_training_matrix, class_training_vector)
+        get_feature_class_matrix([0 + class_ind * validating_len, validating_len * (1 + class_ind)],
+                                 path, file_name_list, feature_validating_matrix, class_validating_vector)
+        get_feature_class_matrix([0 + class_ind * testing_len, testing_len * (1 + class_ind)],
+                                 path, file_name_list, feature_testing_matrix, class_testing_vector)
+
+    return feature_training_matrix, class_training_vector, \
+           feature_validating_matrix, class_validating_vector, \
+           feature_testing_matrix, class_testing_vector
+
+
 if __name__ == '__main__':
-    print get_data()
-    # print math.ceil(4.2)
-    # print math.floor(4.2)
+
+    print get_data([0.8, 0.1, 0.1])
