9e078123a71bc3f91916fdd7a170a15bf303d7ad
Hugh Wilson <hughwilson1@hotmail.co.uk>
2016-02-21 20:40:59 -0500

    Four classifiers

--- classifiers/first_classifiers.py
+++ classifiers/first_classifiers.py
@@ -8,6 +8,9 @@ into classes using a set of vectors and lables
 import numpy as np
 from sklearn import datasets
 from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier
 import catch_errors as cer
 import simple_validate as sv
 
@@ -17,12 +20,51 @@ def gaussNB(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
     applies it to Xvl_tst and compares the results to Yvl_tst using
     validate().'''
     cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
-    gnb = GaussianNB()
-    gnb.fit(Xtrn, Ytrn)
-    Y_pred = gnb.predict(Xvl_tst)
+    gaussnb = GaussianNB()
+    gaussnb.fit(Xtrn, Ytrn)
+    Y_pred = gaussnb.predict(Xvl_tst)
+    return(sv.validate(Y_pred,Yvl_tst))
+
+def QuadDiscAnal(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
+    '''Trains a Quadratic Discriminant Analysis classifier on Xtrn and Ytrn;
+    applies it to Xvl_tst and compares the results to Yvl_tst using
+    validate().'''
+    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
+    QDA = QuadraticDiscriminantAnalysis()
+    QDA.fit(Xtrn,Ytrn)
+    Y_pred = QDA.predict(Xvl_tst)
+    return(sv.validate(Y_pred,Yvl_tst))
+
+def DecisionTree(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
+    '''Trains a DecisionTree classifier on Xtrn and Ytrn;
+    applies it to Xvl_tst and compares the results to Yvl_tst using
+    validate().'''
+    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
+    DTree = tree.DecisionTreeClassifier()
+    DTree.fit(Xtrn,Ytrn)
+    Y_pred = DTree.predict(Xvl_tst)
+    return(sv.validate(Y_pred,Yvl_tst))
+
+def NearestNeighbors(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
+    '''Trains a Nearest Neighbor classifier on Xtrn and Ytrn;
+    applies it to Xvl_tst and compares the results to Yvl_tst using
+    validate().'''
+    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
+    NNs = KNeighborsClassifier(n_neighbors=3, algorithm='auto', \
+                               weights = 'uniform')
+    NNs.fit(Xtrn, Ytrn)
+    Y_pred = NNs.predict(Xvl_tst)
     return(sv.validate(Y_pred,Yvl_tst))
 
 if __name__ == '__main__':
     # some test things
     iris = datasets.load_iris()
-    print(gaussNB(iris.data,iris.target,iris.data,iris.target))
+    print("Gaussian Naive Bayes: " + \
+          str(gaussNB(iris.data,iris.target,iris.data,iris.target)))
+    print("Quadratic Discriminant Analysis: " + \
+          str(QuadDiscAnal(iris.data,iris.target,iris.data,iris.target)))
+    print("Decision Tree: " + \
+          str(DecisionTree(iris.data,iris.target,iris.data,iris.target)))
+    print("Nearest Neighbors: " + \
+          str(NearestNeighbors(iris.data,iris.target,iris.data,iris.target)))
+
