2f876062d152d76a7388b3aa0393a84e25114372
Hugh Wilson <hughwilson1@hotmail.co.uk>
2016-02-22 22:35:45 -0500

    changed the classifier functions to return a vector of scores from cross validation and an instance of the classifier trained on the entire cross validation dataset.

--- classifiers/catch_errors.py
+++ classifiers/catch_errors.py
@@ -6,26 +6,26 @@ Test for errors in the use of the classifier functions
 
 import numpy as np
 
-def catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
+def catch_input_error(Xtrn,Ytrn):
     '''Checks that the inputs have the correct shape. Specifically that the
     Ys are nx1 and the Xs are nxm for n,m integer'''
     assert Xtrn.shape[0] == Ytrn.shape[0], '''Xtrn & Ytrn should have the
     same number of rows'''
-    assert Xvl_tst.shape[0] == Yvl_tst.shape[0], ''' Xvl_tst & Yvl_tst should have the
-    same number of rows'''
+#    assert Xvl_tst.shape[0] == Yvl_tst.shape[0], ''' Xvl_tst & Yvl_tst should have the
+#    same number of rows'''
     assert Xtrn.ndim > 1, '''Xtrn should have a non-zero number of
     columns'''
-    assert Xvl_tst.ndim > 1, '''Xvl_tst should have a non-zero number
-    of columns'''
+#    assert Xvl_tst.ndim > 1, '''Xvl_tst should have a non-zero number
+#    of columns'''
     assert Ytrn.ndim == 1, '''Ytrn should have one column'''
-    assert Yvl_tst.ndim == 1, '''Yvl_tst should have one column'''
+#    assert Yvl_tst.ndim == 1, '''Yvl_tst should have one column'''
 
 if __name__ == '__main__':
     # Test that it catches the errors
     from numpy import array as ar
     a = [1,1]
     print("Correct call of the function")
-    catch_input_error(ar([a,a]),ar([1,1]),ar([a,a]),ar([1,1]))
+    catch_input_error(ar([a,a]),ar([1,1]))
 
 
 
--- classifiers/first_classifiers.py
+++ classifiers/first_classifiers.py
@@ -14,92 +14,96 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 import catch_errors as cer
+from sklearn import cross_validation
 import simple_test as sv
 from data_processing import data_preparation as datprep
 
 # Define the classifier functions
-def gaussNB(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
-    '''Trains a Gaussian naive bayesian classifier on Xtrn and Ytrn;
-    applies it to Xvl_tst and compares the results to Yvl_tst using
-    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)'''
-    gaussnb = GaussianNB()
-    gaussnb.fit(Xtrn, Ytrn)
-    Y_pred = gaussnb.predict(Xvl_tst)
-    return(sv.test(Y_pred,Yvl_tst))
+def gaussNB(Xcvfit,Ycvfit):
+    '''For the Naive Gaussian Bayes classifier, returns
+    a vector of scores from cross validation and an instance
+    of the classifier fit to the cross validation data'''
+    cer.catch_input_error(Xcvfit,Ycvfit)
+    clf = GaussianNB()
+    scores = cross_validation.cross_val_score(clf, Xcvfit, \
+                                              Ycvfit, cv=5)
+    clf.fit(Xcvfit,Ycvfit)
+    return(scores,clf)
 
-def QuadDiscAnal(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
-    '''Trains a Quadratic Discriminant Analysis classifier on Xtrn and Ytrn;
-    applies it to Xvl_tst and compares the results to Yvl_tst using
-    test().'''
-    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
-    QDA = QuadraticDiscriminantAnalysis()
-    QDA.fit(Xtrn,Ytrn)
-    Y_pred = QDA.predict(Xvl_tst)
-    return(sv.test(Y_pred,Yvl_tst))
+def QuadDiscAnal(Xcvfit,Ycvfit):
+    '''For the Quadratic Discriminant Analysis classifier, returns
+    a vector of scores from cross validation and an instance
+    of the classifier fit to the cross validation data'''
+    cer.catch_input_error(Xcvfit,Ycvfit)
+    clf = QuadraticDiscriminantAnalysis()
+    scores = cross_validation.cross_val_score(clf, Xcvfit, \
+                                              Ycvfit, cv=5)
+    clf.fit(Xcvfit,Ycvfit)
+    return(scores,clf)
 
-def DecisionTree(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
-    '''Trains a DecisionTree classifier on Xtrn and Ytrn;
-    applies it to Xvl_tst and compares the results to Yvl_tst using
-    test().'''
-    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
-    DTree = tree.DecisionTreeClassifier()
-    DTree.fit(Xtrn,Ytrn)
-    Y_pred = DTree.predict(Xvl_tst)
-    return(sv.test(Y_pred,Yvl_tst))
+def DecisionTree(Xcvfit,Ycvfit):
+    '''For the Decision Tree classifier, returns
+    a vector of scores from cross validation and an instance
+    of the classifier fit to the cross validation data'''
+    cer.catch_input_error(Xcvfit,Ycvfit)
+    clf = tree.DecisionTreeClassifier()
+    scores = cross_validation.cross_val_score(clf, Xcvfit, \
+                                              Ycvfit, cv=5)
+    clf.fit(Xcvfit,Ycvfit)
+    return(scores,clf)
 
-def NearestNeighbors(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
-    '''Trains a Nearest Neighbor classifier on Xtrn and Ytrn;
-    applies it to Xvl_tst and compares the results to Yvl_tst using
-    test().'''
-    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
-    NNs = KNeighborsClassifier(n_neighbors=3, algorithm='auto', \
+def NearestNeighbors(Xcvfit,Ycvfit):
+    '''For the Nearest Neighbors classifier, returns
+    a vector of scores from cross validation and an instance
+    of the classifier fit to the cross validation data'''
+    cer.catch_input_error(Xcvfit,Ycvfit)
+    clf = KNeighborsClassifier(n_neighbors=3, algorithm='auto', \
                                weights = 'uniform')
-    NNs.fit(Xtrn, Ytrn)
-    Y_pred = NNs.predict(Xvl_tst)
-    return(sv.test(Y_pred,Yvl_tst))
+    scores = cross_validation.cross_val_score(clf, Xcvfit, \
+                                              Ycvfit, cv=5)
+    clf.fit(Xcvfit,Ycvfit)
+    return(scores,clf)
 
-def RandomForrest(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
-    '''Trains a Random Forrest classifier on Xtrn and Ytrn;
-    applies it to Xvl_tst and compares the results to Yvl_tst using
-    test().'''
-    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
-    RandF = RandomForestClassifier(n_estimators = 10)
-    RandF.fit(Xtrn, Ytrn)
-    Y_pred = RandF.predict(Xvl_tst)
-    return(sv.test(Y_pred,Yvl_tst))
+def RandomForrest(Xcvfit,Ycvfit):
+    '''For the Random Forrest classifier, returns
+    a vector of scores from cross validation and an instance
+    of the classifier fit to the cross validation data'''
+    cer.catch_input_error(Xcvfit,Ycvfit)
+    clf = RandomForestClassifier(n_estimators = 10)
+    scores = cross_validation.cross_val_score(clf, Xcvfit, \
+                                              Ycvfit, cv=5)
+    clf.fit(Xcvfit,Ycvfit)
+    return(scores,clf)
 
-def AdaBoost(Xtrn,Ytrn,Xvl_tst,Yvl_tst):
-    '''Trains an Ada Boost classifier on Xtrn and Ytrn;
-    applies it to Xvl_tst and compares the results to Yvl_tst using
-    test().'''
-    cer.catch_input_error(Xtrn,Ytrn,Xvl_tst,Yvl_tst)
-    AdaB = AdaBoostClassifier(n_estimators = 100)
-    AdaB.fit(Xtrn, Ytrn)
-    Y_pred = AdaB.predict(Xvl_tst)
-    return(sv.test(Y_pred,Yvl_tst))
+def AdaBoost(Xcvfit,Ycvfit):
+    '''For the AdaBoost classifier, returns
+    a vector of scores from cross validation and an instance
+    of the classifier fit to the cross validation data'''
+    cer.catch_input_error(Xcvfit,Ycvfit)
+    clf = AdaBoostClassifier(n_estimators = 100)
+    scores = cross_validation.cross_val_score(clf, Xcvfit, \
+                                              Ycvfit, cv=5)
+    clf.fit(Xcvfit,Ycvfit)
+    return(scores,clf)
 
 if __name__ == '__main__':
+    pass
     # some test things
-    feat_train, label_train, feat_val, label_val, feat_test, label_test = \
-        datprep.get_data([0.8, 0.1, 0.1], feature='zerocross')
-    print feat_train
-    # arg_list = [feat_train,label_train,feat_test,label_test]
-    # print("Gaussian Naive Bayes: " + \
-    #       str(gaussNB(*arg_list)))
-    # print("Quadratic Discriminant Analysis: " + \
-    #       str(QuadDiscAnal(*arg_list)))
-    # print("Decision Tree: " + \
-    #       str(DecisionTree(*arg_list)))
-    # print("Nearest Neighbors: " + \
-    #       str(NearestNeighbors(*arg_list)))
-    # print("Random Forrest: " + \
-    #       str(RandomForrest(*arg_list)))
-    # print("Ada Boost: " + \
-    #       str(AdaBoost(*arg_list)))
-
-
-
-
+#    feat_train, label_train, feat_val, label_val, feat_test, label_test = \
+#        datprep.get_data([0.8, 0.1, 0.1], feature='zerocross')
+#    arg_list = [feat_train,label_train,feat_test,label_test]
+#    print("Gaussian Naive Bayes: " + \
+#          str(gaussNB(*arg_list)))
+#    print("Quadratic Discriminant Analysis: " + \
+#          str(QuadDiscAnal(*arg_list)))
+#    print("Decision Tree: " + \
+#          str(DecisionTree(*arg_list)))
+#    print("Nearest Neighbors: " + \
+#          str(NearestNeighbors(*arg_list)))
+#    print("Random Forrest: " + \
+#          str(RandomForrest(*arg_list)))
+#    print("Ada Boost: " + \
+#          str(AdaBoost(*arg_list)))
 
 #    iris = datasets.load_iris()
 #    print("Gaussian Naive Bayes: " + \
@@ -113,5 +117,4 @@ if __name__ == '__main__':
 #    print("Random Forrest: " + \
 #          str(RandomForrest(iris.data,iris.target,iris.data,iris.target)))
 #    print("Ada Boost: " + \
-#          str(AdaBoost(iris.data,iris.target,iris.data,iris.target)))
-#
+#          str(AdaBoost(iris.data,iris.target,iris.data,iris.target))
